fa54740afab8301aee534d54ca19f71c9da3b455
Thaqif Rosdi <70655268+takippu@users.noreply.github.com>
2022-11-26 23:02:59 +0800

    Install dependencies

---
+++ voicesofump/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+
+    "./resources/**/*.{blade.php,vue,js,html}",
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
