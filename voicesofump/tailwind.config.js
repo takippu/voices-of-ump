69e6be2615db5c736c6c820da6b601e2b7e576cf
Thaqif Rosdi <70655268+takippu@users.noreply.github.com>
2022-11-27 12:39:17 +0800

    Installed jetstream

--- voicesofump/tailwind.config.js
+++ voicesofump/tailwind.config.js
@@ -1,20 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    fontFamily: {
-      Mukta: ["Mukta"],
-      Roboto: ["Roboto"],
-      Glitch: ["Rubik Glitch"],
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-    container:{
-      center: true,
-    },
-    extend: {},
-  },
-  plugins: [],
-}
+
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
