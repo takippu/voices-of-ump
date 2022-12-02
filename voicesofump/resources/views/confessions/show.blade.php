e83d78e362398a0d85502ac0dc13e2ca54200ed0
Thaqif Rosdi <70655268+takippu@users.noreply.github.com>
2022-12-02 09:12:00 +0800

    fixnav bar

--- voicesofump/resources/views/confessions/show.blade.php
+++ voicesofump/resources/views/confessions/show.blade.php
@@ -37,20 +37,26 @@
           
 
           <!-- component -->
-<article class="py-12 px-4">
+<article class="py-12 px-4 ">
   <h1 class="text-4xl text-center mb-4 font-semibold font-heading font-semibold">{{$confessions->title}}</h1>
+  
   <p class="text-center">
+
     <span>{{$confessions->created_at}}, by</span>
-    <a class="ml-1 text-indigo-600 hover:underline" href="#">{{ucFirst($confessions->user->name)}}</a>
-    
+    <a class="ml-1 text-indigo-600 hover:underline" href="#">{{ucFirst($confessions->user->name)}}    
+      
+    </a>
+    @if(auth()->user()->id == $confessions->user_id)
+      
+        <button onclick="window.location='/confessions/{{$confessions->id}}/edit'" class="bg-blue-500 hover:bg-blue-700 text-white text-xs font-bold py-2 px-2 rounded ">
+            Edit Post
+          </button>
+         <!-- <a class="text-center" href="/confessions/{{$confessions->id}}/edit">[Edit]</a> -->
+      
+      @endif
   </p>
-  @if(auth()->user()->id == $confessions->user_id)
-    <div class="items-end flex justify-between">
-      <button onclick="window.location='/confessions/{{$confessions->id}}/edit'" class="bg-blue-500 hover:bg-blue-700 text-white text-xs font-bold py-2 px-2 rounded ">
-          Edit Post
-        </button>
-    </div>
-    @endif
+  
+
   <div class="max-w-3xl mx-auto">
     {!! $confessions->content !!}
   </div>
--- voicesofump/resources/views/layouts/app.blade.php
+++ voicesofump/resources/views/layouts/app.blade.php
@@ -6,6 +6,9 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        @vite('resources/css/app.css')
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -14,22 +17,23 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Styles -->
+        @vite('resources/css/app.css')
         @livewireStyles
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen">
             @livewire('navigation-menu')
 
             <!-- Page Heading -->
-            @if (isset($header))
+            {{--@if (isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endif
+            @endif--}}
 
             <!-- Page Content -->
             <main>
