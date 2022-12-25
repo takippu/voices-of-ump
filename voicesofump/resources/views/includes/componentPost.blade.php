beba36680fc4a2ae87e9fa10233a212d27e97dc6
Thaqif Rosdi <70655268+takippu@users.noreply.github.com>
2022-12-25 23:04:49 +0800

    comment count
    make it so that comment count for each post

--- voicesofump/resources/views/includes/componentPost.blade.php
+++ voicesofump/resources/views/includes/componentPost.blade.php
@@ -68,7 +68,9 @@
                     d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
                   ></path>
                 </svg>
-                <span>25</span>
+                <span>{{
+                $post->comments->count()
+                }}</span> <!-- count the comment -->
               </div>
     
               <div
