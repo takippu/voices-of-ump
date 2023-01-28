29edd8b445c0fcdfc2aadf54ea4f16329cacfd33
Thaqif Rosdi <70655268+takippu@users.noreply.github.com>
2023-01-29 01:28:23 +0800

    fix image on show petition

--- voicesofump/resources/views/petitions/show.blade.php
+++ voicesofump/resources/views/petitions/show.blade.php
@@ -30,9 +30,16 @@
 
             <article class="flex flex-col shadow my-4">
                 <!-- Article Image -->
-                <a href="#" class="hover:opacity-75">
-                    <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1">
-                </a>
+
+                    <a href="#" class="hover:opacity-75">
+                        @if (empty($petitions->image_path))
+                        <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1">
+                        @else
+                        <img src="{{asset('/'.$petitions->image_path)}}" class="object-fill h-screen w-screen">
+                        @endif
+                    </a>
+
+
                 <div class="bg-white flex flex-col justify-start p-6">
                     <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">PETITION</a>
                     <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">{{$petitions->title}}</a>
